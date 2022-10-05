37120bf386fb5df61fbfbed34c4df523cb45d841
Georgeton <georgeton.santos.santana@gmail.com>
2022-10-05 15:01:39 -0300

    Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from'@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -26,6 +26,7 @@ import { FramePageComponent } from './pages/master/frame.page';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
